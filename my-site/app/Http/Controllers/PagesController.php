57e1f4de445c8c1aff73493b100b10181f93d563
Gabriel Cesar <gcal1@live.com>
2018-01-28 02:11:08 -0300

    create new pages and add routers

--- my-site/app/Http/Controllers/PagesController.php
+++ my-site/app/Http/Controllers/PagesController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function home() {
-        return view('welcome');
+        return view('home');
+    }
+
+    public function about () {
+        return view('about');
+    }
+
+    public function contact () {
+        return view('contact');
     }
 }
--- my-site/routes/web.php
+++ my-site/routes/web.php
@@ -12,3 +12,5 @@
 */
 
 Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'PagesController@contact');
