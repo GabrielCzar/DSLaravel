5a0882a659d892ac26be69e5aefa8f645c8c7d04
Gabriel Cesar <gcal1@live.com>
2018-01-28 02:53:28 -0300

    add material bootstrap and working with blade templates

--- my-site/resources/views/home.blade.php
+++ my-site/resources/views/home.blade.php
@@ -1,48 +1,27 @@
-<html>
-<head>
-    <title>Home Page</title>
+@extends('master')
 
-    <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+@section('title', 'Home')
 
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            height: 100%;
-            color: #B0BEC5;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
+@section('content')
 
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
+    <div class="container">
+        <div class="row banner">
 
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
+            <div class="col-md-12">
 
-        .title {
-            font-size: 96px;
-            margin-bottom: 40px;
-        }
+                <h1 class="text-center margin-top-100 editContent">
+                    Learning Laravel 5
+                </h1>
 
-        .quote {
-            font-size: 24px;
-        }
-    </style>
-</head>
-<body>
-<div class="container">
-    <div class="content">
-        <div class="title">Home Page</div>
-        <div class="quote">Our Home page!</div>
+                <h3 class="text-center margin-top-100 editContent">Building Practical Applications</h3>
+
+                <div class="text-center">
+                    <img src="https://learninglaravel.net/img/LearningLaravel5_cover0.png" width="302" height="391" alt="">
+                </div>
+
+            </div>
+
+        </div>
     </div>
-</div>
-</body>
-</html>
+
+@endsection
