d1a51567f02c58247f370d87c446565eea147f75
Gabriel Cesar <gcal1@live.com>
2018-01-31 02:48:10 -0300

    send emails

---
+++ support-ticket-system/resources/views/emails/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h2>Learning Laravel!</h2>
+
+<div>
+    Welcome to {!! $name !!} website!
+
+    <!--<?php echo $name ?>-->
+</div>
+
+</body>
+</html>
--- support-ticket-system/routes/web.php
+++ support-ticket-system/routes/web.php
@@ -26,3 +26,22 @@ Route::get('/ticket/{slug?}', 'TicketsController@show');
 Route::get('/ticket/{slug?}/edit','TicketsController@edit');
 Route::post('/ticket/{slug?}/edit','TicketsController@update');
 Route::post('/ticket/{slug?}/delete','TicketsController@destroy');
+
+// Email
+Route::get('sendemail', function () {
+
+    $data = array(
+        'name' => "Learning Laravel",
+    );
+
+    Mail::send('emails.welcome', $data, function ($message) {
+
+        $message->from('thelegend@gmail.com', 'Learning Laravel');
+
+        $message->to('gabrielcesar.a.l@gmail.com')->subject('Learning Laravel test email');
+
+    });
+
+    return "Your email has been sent successfully";
+
+});
